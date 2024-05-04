97f5a28e9cf2b7327205b05f67162bcd23389bf6
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-05-04 18:34:53 +0600

    funtion with generics

---
+++ 2.5.ts
@@ -0,0 +1,29 @@
+{
+    function with generics
+    const createArray = (params: string): string[] => {
+        return [params];
+    }
+    const res1 = createArray("bangladesh");
+    const createArrayWithG = <T>(params: T): T[] => {
+        return [params]
+    }
+    type user = { id: number; name: string; }
+
+    const res2 = createArrayWithG<user>({
+        id: 222,
+        name: "mr. x"
+    })
+
+    const addCourseToStdent = <T>(student: T) => {
+        const course = 'next level web developer'
+
+        return {
+            ...student,
+            course
+        }
+    }
+
+    const student1 = addCourseToStdent({ name: 'x', email: "x@gmail.com", devType: 'nlwd' })
+
+    console.log(student1);
+}
